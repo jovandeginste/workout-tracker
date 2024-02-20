298b7183e68ac6f6801d1e69b4ac39685c78c82b
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2024-02-20 12:36:47 +0100

    Some more tests
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- pkg/templatehelpers/icons_test.go
+++ pkg/templatehelpers/icons_test.go
@@ -10,4 +10,6 @@ func TestIconFor(t *testing.T) {
 	assert.Equal(t, "icon-solid icon-baseline icon-space-sm icon-before icon-question", IconFor(""))
 	assert.Contains(t, IconFor("distance"), "icon-road")
 	assert.Contains(t, IconFor("close"), "icon-xmark")
+	assert.Contains(t, IconFor("dashboard"), "icon-chart-line")
+	assert.Contains(t, IconFor("running"), "icon-person-running")
 }
