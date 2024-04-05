75e482c79f2d15456e2f9f9dfaf962d22336ee03
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2024-04-05 20:36:51 +0200

    Add two new API endpoints for workout retrieval
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- pkg/app/api_handlers.go
+++ pkg/app/api_handlers.go
@@ -3,6 +3,7 @@ package app
 import (
 	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/jovandeginste/workout-tracker/pkg/database"
 	"github.com/labstack/echo/v4"
@@ -11,9 +12,22 @@ import (
 
 var ErrInvalidAPIKey = errors.New("invalid API key")
 
+type APIResponse struct {
+	Errors  []string    `json:"errors"`
+	Results interface{} `json:"results"`
+}
+
 func (a *App) apiRoutes(e *echo.Group) {
 	apiGroup := e.Group("/api/v1")
-	apiGroup.Use(middleware.KeyAuth(func(key string, c echo.Context) (bool, error) {
+	apiGroup.Use(a.ValidateAPIKeyMiddleware())
+
+	apiGroup.GET("/whoami", a.apiWhoamiHandler).Name = "api-whoami"
+	apiGroup.GET("/workouts", a.apiWorkoutsHandler).Name = "api-workouts"
+	apiGroup.GET("/workout/:id", a.apiWorkoutHandler).Name = "api-workout"
+}
+
+func (a *App) ValidateAPIKeyMiddleware() echo.MiddlewareFunc {
+	return middleware.KeyAuth(func(key string, c echo.Context) (bool, error) {
 		u, err := database.GetUserByAPIKey(a.db, key)
 		if err != nil {
 			return false, ErrInvalidAPIKey
@@ -28,13 +42,42 @@ func (a *App) apiRoutes(e *echo.Group) {
 		c.Set("user_totals_show", u.Profile.TotalsShow)
 
 		return true, nil
-	}))
-
-	apiGroup.GET("/whoami", a.apiWhoamiHandler).Name = "api-whoami"
+	})
 }
 
 func (a *App) apiWhoamiHandler(c echo.Context) error {
 	data := a.defaultData(c)
 
 	return c.JSON(http.StatusOK, data)
+}
+
+func (a *App) apiWorkoutsHandler(c echo.Context) error {
+	resp := APIResponse{}
+
+	w, err := a.getCurrentUser(c).GetWorkouts(a.db)
+	if err != nil {
+		resp.Errors = append(resp.Errors, err.Error())
+	}
+
+	resp.Results = w
+
+	return c.JSON(http.StatusOK, resp)
+}
+
+func (a *App) apiWorkoutHandler(c echo.Context) error {
+	resp := APIResponse{}
+
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return a.redirectWithError(c, "/workouts", err)
+	}
+
+	w, err := a.getCurrentUser(c).GetWorkout(a.db, id)
+	if err != nil {
+		resp.Errors = append(resp.Errors, err.Error())
+	}
+
+	resp.Results = w
+
+	return c.JSON(http.StatusOK, resp)
 }
--- pkg/database/user.go
+++ pkg/database/user.go
@@ -207,7 +207,7 @@ func (u *User) Delete(db *gorm.DB) error {
 func (u *User) GetWorkout(db *gorm.DB, id int) (*Workout, error) {
 	var w *Workout
 
-	if err := db.Preload("Data").Preload("GPX").Where(&Workout{UserID: u.ID}).First(&w, id).Error; err != nil {
+	if err := db.Preload("Data").Where(&Workout{UserID: u.ID}).First(&w, id).Error; err != nil {
 		return nil, err
 	}
 
