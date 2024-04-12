9418096e9b6e58e832fc64a05e0fa8f697b356ea
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2024-04-12 08:38:34 +0200

    Fix the tests for workout statistics
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- pkg/database/workouts_statistics_test.go
+++ pkg/database/workouts_statistics_test.go
@@ -25,56 +25,56 @@ func TestStatisticsItem_CanHave(t *testing.T) {
 		Counter:       2,
 	}
 
-	assert.True(t, si.canHaveDistance(100))
-	assert.True(t, si.canHaveDistance(199))
-	assert.False(t, si.canHaveDistance(200))
-	assert.False(t, si.canHaveDistance(203))
+	assert.True(t, si.canHaveDistance(100, 2000))
+	assert.True(t, si.canHaveDistance(199, 2000))
+	assert.False(t, si.canHaveDistance(200, 2000))
+	assert.False(t, si.canHaveDistance(203, 2000))
 }
 
 func TestStatisticsItem_StatisticsPerKilometer(t *testing.T) {
 	w := defaultWorkout(t)
 	assert.NotNil(t, w)
 
-	stats := w.StatisticsPerKilometer()
-	assert.Len(t, stats, 4)
+	stats := w.StatisticsPer(1, "km")
+	assert.Len(t, stats.Items, 4)
 
-	assert.Equal(t, 1, stats[0].Counter)
-	assert.Equal(t, 2, stats[1].Counter)
-	assert.Equal(t, 4, stats[3].Counter)
+	assert.Equal(t, 1, stats.Items[0].Counter)
+	assert.Equal(t, 2, stats.Items[1].Counter)
+	assert.Equal(t, 4, stats.Items[3].Counter)
 
-	assert.InDelta(t, 1000, stats[0].TotalDistance, 20)
-	assert.InDelta(t, 2000, stats[1].TotalDistance, 20)
-	assert.InDelta(t, 3100, stats[3].TotalDistance, 20)
+	assert.InDelta(t, 1000, stats.Items[0].TotalDistance, 20)
+	assert.InDelta(t, 2000, stats.Items[1].TotalDistance, 20)
+	assert.InDelta(t, 3100, stats.Items[3].TotalDistance, 20)
 
-	assert.Equal(t, 242*time.Second, stats[0].Duration)
-	assert.Equal(t, 336*time.Second, stats[1].Duration)
-	assert.Equal(t, 31*time.Second, stats[3].Duration)
+	assert.Equal(t, 242*time.Second, stats.Items[0].Duration)
+	assert.Equal(t, 336*time.Second, stats.Items[1].Duration)
+	assert.Equal(t, 31*time.Second, stats.Items[3].Duration)
 }
 
 func TestStatisticsItem_StatisticsPerMinute(t *testing.T) {
 	w := defaultWorkout(t)
 	assert.NotNil(t, w)
 
-	stats := w.StatisticsPerMinute()
-	assert.Len(t, stats, 16)
+	stats := w.StatisticsPer(1, "min")
+	assert.Len(t, stats.Items, 16)
 
-	assert.Equal(t, 1, stats[0].Counter)
-	assert.Equal(t, 3, stats[2].Counter)
-	assert.Equal(t, 8, stats[7].Counter)
-	assert.Equal(t, 16, stats[15].Counter)
+	assert.Equal(t, 1, stats.Items[0].Counter)
+	assert.Equal(t, 3, stats.Items[2].Counter)
+	assert.Equal(t, 8, stats.Items[7].Counter)
+	assert.Equal(t, 16, stats.Items[15].Counter)
 
-	assert.InDelta(t, 180, stats[0].TotalDistance, 20)
-	assert.InDelta(t, 690, stats[2].TotalDistance, 20)
-	assert.InDelta(t, 1765, stats[7].TotalDistance, 20)
-	assert.InDelta(t, 3100, stats[15].TotalDistance, 20)
+	assert.InDelta(t, 180, stats.Items[0].TotalDistance, 20)
+	assert.InDelta(t, 690, stats.Items[2].TotalDistance, 20)
+	assert.InDelta(t, 1765, stats.Items[7].TotalDistance, 20)
+	assert.InDelta(t, 3100, stats.Items[15].TotalDistance, 20)
 
-	assert.Equal(t, 56*time.Second, stats[0].Duration)
-	assert.Equal(t, 57*time.Second, stats[1].Duration)
-	assert.Equal(t, 106*time.Second, stats[7].Duration)
-	assert.Equal(t, 31*time.Second, stats[15].Duration)
+	assert.Equal(t, 56*time.Second, stats.Items[0].Duration)
+	assert.Equal(t, 57*time.Second, stats.Items[1].Duration)
+	assert.Equal(t, 106*time.Second, stats.Items[7].Duration)
+	assert.Equal(t, 31*time.Second, stats.Items[15].Duration)
 
-	assert.Equal(t, 56*time.Second, stats[0].TotalDuration)
-	assert.Equal(t, 113*time.Second, stats[1].TotalDuration)
-	assert.Equal(t, 509*time.Second, stats[7].TotalDuration)
-	assert.Equal(t, 948*time.Second, stats[15].TotalDuration)
+	assert.Equal(t, 56*time.Second, stats.Items[0].TotalDuration)
+	assert.Equal(t, 113*time.Second, stats.Items[1].TotalDuration)
+	assert.Equal(t, 509*time.Second, stats.Items[7].TotalDuration)
+	assert.Equal(t, 948*time.Second, stats.Items[15].TotalDuration)
 }
