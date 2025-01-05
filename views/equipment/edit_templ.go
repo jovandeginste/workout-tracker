3eed0930b2b78d80b958a96c2d2a85341652dd34
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2025-01-05 17:44:22 +0100

    Add helper for default value for selectable items
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- views/equipment/edit_templ.go
+++ views/equipment/edit_templ.go
@@ -234,9 +234,9 @@ func Edit(e *database.Equipment) templ.Component {
 				return templ_7745c5c3_Err
 			}
 			var templ_7745c5c3_Var14 string
-			templ_7745c5c3_Var14, templ_7745c5c3_Err = templ.JoinStringErrs(wt.String())
+			templ_7745c5c3_Var14, templ_7745c5c3_Err = templ.JoinStringErrs(helpers.THas(ctx, wt.String()))
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/equipment/edit.templ`, Line: 85, Col: 28}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/equipment/edit.templ`, Line: 85, Col: 47}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var14))
 			if templ_7745c5c3_Err != nil {
