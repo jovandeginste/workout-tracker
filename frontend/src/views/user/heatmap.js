623bee38daf5e75d943dfdd6ee6a7679b6cfeff2
Harold Cheng <niuchangcun@gmail.com>
2025-08-14 00:41:16 +0800

    feat: user can control some options in heatmap

--- frontend/src/views/user/heatmap.js
+++ frontend/src/views/user/heatmap.js
@@ -62,9 +62,58 @@ class WtHeatmap extends HTMLElement {
       })
       .addTo(map);
 
+    let heatLayer = null;
+    let heatMapData = null;
+    let markers = null;
+    const rerenderHeatMap = () => {
+      if (heatMapData === null || markers === null) {
+        console.log("data not ready")
+        return
+      }
+      if (heatLayer !== null) {
+        map.removeLayer(heatLayer);
+      }
+      const radius = L.DomUtil.get("radius").value;
+      const blur = L.DomUtil.get("blur").value;
+      const showMarkers = L.DomUtil.get("showMarkers").checked;
+      heatLayer = L.heatLayer(heatMapData, { radius: Number(radius), blur: Number(blur) })
+      heatLayer.addTo(map);
+      if (showMarkers) {
+        markers.addTo(map);
+      } else {
+        markers.removeFrom(map);
+      }
+    }
+
+    let customControl = L.Control.extend({
+      options: { position: 'topright' },
+
+      onAdd: function () {
+        const container = L.DomUtil.create('div', 'flex flex-col p-2');
+
+        container.style.backgroundColor = 'white';
+        container.innerHTML = `
+        <div class="flex items-center"><label for="radius" class="w-12">Radius </label><input class="p-0" type="range" id="radius" value="10" min="5" max="30"/></div>
+        <div class="flex items-center"><label for="blur" class="w-12">Blur </label><input class="p-0" type="range" id="blur" value="15" min="5" max="30"/></div>
+        <div class="flex items-center"><input type="checkbox" id="showMarkers" name="showMarkers" checked /><label for="showMarkers">Show Markers</label></div>
+        `;
+
+        // Prevent map drag when clicking control
+        L.DomEvent.disableClickPropagation(container);
+
+        container.querySelectorAll("input")
+          .forEach((element) => {
+            element.oninput = L.Util.throttle(rerenderHeatMap, 50);
+          })
+
+        return container;
+      }
+    });
+
+    map.addControl(new customControl());
+
     layerStreet.addTo(map);
 
-    var heatConfig = { radius: 10 };
     var clusterConfig = { showCoverageOnHover: false };
 
     fetch(this.apiWorkoutsCoordinatesRoute, {
@@ -75,8 +124,8 @@ class WtHeatmap extends HTMLElement {
     })
       .then((response) => response.json())
       .then((response) => {
-        var data = geoJson2heat(response.results);
-        L.heatLayer(data, heatConfig).addTo(map);
+        heatMapData = geoJson2heat(response.results);
+        rerenderHeatMap();
       });
 
     fetch(this.apiWorkoutsCentersRoute, {
@@ -87,8 +136,8 @@ class WtHeatmap extends HTMLElement {
     })
       .then((response) => response.json())
       .then((response) => {
-        var markers = L.markerClusterGroup(clusterConfig);
-        var geoJsonLayer = L.geoJson(response.results, {
+        markers = L.markerClusterGroup(clusterConfig);
+        const geoJsonLayer = L.geoJson(response.results, {
           onEachFeature: function (feature, layer) {
             layer.bindPopup(feature.properties.details);
           },
