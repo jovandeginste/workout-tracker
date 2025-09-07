71de2a3cdc4d9252e60581f03aef7612a8cc4812
Bastian Rihm <me@bastianjoel.de>
2025-09-07 21:20:51 +0200

    feat(workout) Add workout chart distance x-axis

--- frontend/src/views/workouts/show_stats.ts
+++ frontend/src/views/workouts/show_stats.ts
@@ -6,6 +6,7 @@ import {
   Chart,
   ChartDataCustomTypesPerDataset,
   ChartDatasetCustomTypesPerDataset,
+  ChartOptions,
   Colors,
   Decimation,
   Filler,
@@ -19,8 +20,13 @@ import {
 } from "chart.js";
 import "chartjs-adapter-date-fns";
 import Zoom from "chartjs-plugin-zoom";
+import { localized, msg } from "@lit/localize";
+import { initLocalize } from "../../locale.js";
+
+initLocalize();
 
 @customElement("workout-stats")
+@localized()
 export class WorkoutStats extends LitElement {
   @property({
     attribute: "color-mode",
@@ -62,6 +68,8 @@ export class WorkoutStats extends LitElement {
   })
   translations = null;
 
+  private timeLabels: number[] = [];
+
   public constructor() {
     super();
 
@@ -82,7 +90,10 @@ export class WorkoutStats extends LitElement {
   }
 
   private chart: Chart | null = null;
-  private chartData: ChartDataCustomTypesPerDataset;
+  private chartData: ChartDataCustomTypesPerDataset = {
+    datasets: [],
+    labels: [],
+  };
 
   public zoomX(start: number | Date, end: number | Date) {
     if (!this.chart) {
@@ -97,7 +108,16 @@ export class WorkoutStats extends LitElement {
       end = end.valueOf();
     }
 
-    this.chart.zoomScale("x", { min: start, max: end });
+    if (this.type === "distance") {
+      start = parseFloat(
+        this.data["distance"].Data[this.timeLabels.indexOf(start)],
+      );
+      end = parseFloat(
+        this.data["distance"].Data[this.timeLabels.indexOf(end)],
+      );
+    }
+
+    this.chart.zoomScale("x", { min: start as number, max: end as number });
   }
 
   public resetZoom() {
@@ -109,196 +129,250 @@ export class WorkoutStats extends LitElement {
   }
 
   public willUpdate(cProps: PropertyValues<this>) {
-    if (!cProps.has("data") && !cProps.has("preferredUnits")) {
-      return;
-    }
-
-    if (this.chart) {
-      this.chart.destroy();
-      this.chart = null;
-    }
-
-    let labels: (number | Date)[];
-    if (this.type === "time") {
-      labels = this.data["time"].Data.map((t: number) => new Date(t).valueOf());
-    } else if (this.type === "distance") {
-      labels = this.data["distance"].Data.map((d: string) => parseFloat(d));
-    }
-
-    const metricSettings = this.getMetricSettings();
-    const datasets: ChartDatasetCustomTypesPerDataset[] = [];
-    for (let metric of Object.keys(metricSettings)) {
-      if (metric === "time") continue;
-      if (metric === "duration") continue;
-      if (metric === "distance") continue;
-      if (this.data[metric] !== undefined) {
-        datasets.push({
-          type: "line",
-          fill: metricSettings[metric].seriesType === "area" ? "start" : false,
-          spanGaps: true,
-          label: this.data[metric].Label,
-          hidden: metricSettings[metric].hiddenByDefault || false,
-          data: this.data[metric].Data,
-          yAxisID: metric,
-        });
+    if (cProps.has("data") || cProps.has("type")) {
+      let labels: (number | Date)[];
+      this.timeLabels = this.data["time"].Data.map((t: number) =>
+        new Date(t).valueOf(),
+      );
+      if (this.type === "time") {
+        labels = this.timeLabels;
+      } else if (this.type === "distance") {
+        labels = this.data["distance"].Data.map((d: string) => parseFloat(d));
       }
+
+      this.chartData.labels = labels;
     }
 
-    this.chartData = {
-      datasets,
-      labels,
-    };
+    if (cProps.has("data") || !cProps.has("preferredUnits")) {
+      const metricSettings = this.getMetricSettings();
+      const datasets: ChartDatasetCustomTypesPerDataset[] = [];
+      for (let metric of Object.keys(metricSettings)) {
+        if (metric === "time") continue;
+        if (metric === "duration") continue;
+        if (metric === "distance") continue;
+        if (this.data[metric] !== undefined) {
+          datasets.push({
+            type: "line",
+            fill:
+              metricSettings[metric].seriesType === "area" ? "start" : false,
+            spanGaps: true,
+            label: this.data[metric].Label,
+            hidden: metricSettings[metric].hiddenByDefault || false,
+            data: this.data[metric].Data,
+            yAxisID: metric,
+          });
+        }
+      }
+
+      this.chartData.datasets = datasets;
+    }
   }
 
   public override updated(props: PropertyValues): void {
     super.updated(props);
 
-    const canvas = this.querySelector("canvas");
-    if (canvas && this.chartData) {
-      const metricSettings = this.getMetricSettings();
-      this.chart = new Chart(canvas, {
-        data: this.chartData,
-        options: {
-          maintainAspectRatio: false,
-          animation: false,
-          scales: {
-            x: {
-              type: this.type === "time" ? "time" : "linear",
-              time: this.type === "time" ? { unit: "minute" } : undefined,
-              min:
-                this.type === "time"
-                  ? new Date(this.data["time"].Data[0]).valueOf()
-                  : parseFloat(this.data["distance"].Data[0]),
-              max:
-                this.type === "time"
-                  ? new Date(
-                      this.data["time"].Data[this.data["time"].Data.length - 1],
-                    ).valueOf()
-                  : parseFloat(
-                      this.data["distance"].Data[
-                        this.data["distance"].Data.length - 1
-                      ],
-                    ),
-              ticks: {
-                callback: (val) => {
-                  return new Date(val as number).toTimeString().substr(0, 5);
-                },
-              },
-            },
-            ...Object.fromEntries(
-              Object.keys(metricSettings)
-                .map((metric) => {
-                  if (metricSettings[metric].yaxis === false) {
-                    return [];
-                  }
-
-                  return [
-                    metric,
-                    {
-                      display: !metricSettings[metric].hiddenByDefault,
-                      position: "left",
-                      ...metricSettings[metric].yaxis,
-                      ticks: {
-                        callback: (val) => {
-                          const settings = metricSettings[metric];
-                          if (settings.formatterYaxis) {
-                            return settings.labelFormatter
-                              ? settings.labelFormatter(val as number)
-                              : settings.formatter(val as number);
-                          }
-                          return val;
-                        },
-                      },
-                    },
-                  ];
-                })
-                .filter((e) => e.length > 0),
-            ),
-          },
-          elements: {
-            point: {
-              radius: 0,
-            },
-          },
-          interaction: {
-            mode: "index",
-            intersect: false,
-          },
-          onHover: (_, i) => {
-            const index = i[0]?.index;
-            let p = this.data["position"].Data[index];
-            let el = document.createElement("div");
-            el.setAttribute("data-lat", p[0]);
-            el.setAttribute("data-lng", p[1]);
-
-            this.map.setMarker(el);
-          },
-          plugins: {
-            decimation: {
-              enabled: true,
-              algorithm: "lttb",
-            },
-            legend: {
-              display: true,
-              onClick: (e, legendItem, legend) => {
-                const meta = legend.chart.getDatasetMeta(
-                  legendItem.datasetIndex,
-                );
-                meta.hidden = meta.hidden === null ? !legendItem.hidden : null;
-                legend.chart.options.scales[meta.yAxisID].display =
-                  legendItem.hidden;
-                legend.chart.update();
-              },
-            },
-            tooltip: {
-              callbacks: {
-                title: (tooltipItems) => {
-                  if (!tooltipItems[0]) {
-                    return;
-                  }
-
-                  const x = tooltipItems[0].parsed.x;
-                  return new Date(x).toTimeString().substr(0, 5);
-                },
-                label: (tooltipItem) => {
-                  const settings = metricSettings[tooltipItem.dataset.yAxisID];
-                  let value = tooltipItem.formattedValue;
-                  if (settings && settings.formatter) {
-                    value = settings.formatter(tooltipItem.raw);
-                  }
-
-                  return `${tooltipItem.dataset.label}: ${value}`;
-                },
-              },
-            },
-            zoom: {
-              limits: {
-                x: { min: "original", max: "original" },
-                y: { min: "original", max: "original" },
-              },
-              zoom: {
-                drag: {
-                  enabled: true,
-                },
-                wheel: {
-                  enabled: true,
-                },
-                mode: "x",
-              },
-            },
-          },
-        },
-      });
+    if (this.chart) {
+      this.chart.options = this.getChartOptions();
+      this.chart.update();
+    } else {
+      const canvas = this.querySelector("canvas");
+      if (canvas && this.chartData) {
+        this.chart = new Chart(canvas, {
+          data: this.chartData,
+          options: this.getChartOptions(),
+        });
+      }
     }
   }
 
   public render(): TemplateResult {
     this.style.display = "block";
-    return html`<canvas></canvas>`;
+    return html`
+        <div class="flex justify-between py-3">
+          <h3>
+            <span>
+              <span class="icon-decoration icon-[fa6-solid--gauge]"></span>
+              ${msg("Average speed", { id: "translation.Average_speed" })}
+            </span>
+            /
+            <span>
+              <span class="icon-decoration icon-[fa6-solid--mountain]"></span>
+              ${msg("Elevation", { id: "translation.Elevation" })}
+            </span>
+          </h3>
+          <nav class="isolate inline-flex">
+            <a
+              href="#"
+              class="relative inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-200 inset-ring inset-ring-gray-700 hover:bg-white/5 focus:z-20 focus:outline-offset-0 ${
+                this.type === "time" ? "bg-indigo-500 text-white" : ""
+              }"
+              @click=${(e: Event) => {
+                e.preventDefault();
+                this.type = "time";
+              }}
+              ><span class="icon-[fa6-regular--clock]"></span></a>
+            <a
+              href="#"
+              class="relative inline-flex items-center px-3 py-2 text-sm font-semibold text-gray-200 inset-ring inset-ring-gray-700 hover:bg-white/5 focus:z-20 focus:outline-offset-0 ${
+                this.type === "distance" ? "bg-indigo-500 text-white" : ""
+              }"
+              @click=${(e: Event) => {
+                e.preventDefault();
+                this.type = "distance";
+              }}
+              ><span class="icon-[fa6-solid--road]"></span></a
+          </nav>
+        </div>
+        <div class="h-[300px] md:h-[400px]">
+          <canvas></canvas>
+        </div>`;
   }
 
   protected createRenderRoot() {
     return this;
+  }
+
+  private getChartOptions(): ChartOptions {
+    const metricSettings = this.getMetricSettings();
+    return {
+      maintainAspectRatio: false,
+      animation: false,
+      scales: {
+        x: {
+          type: this.type === "time" ? "time" : "linear",
+          time: this.type === "time" ? { unit: "minute" } : undefined,
+          min:
+            this.type === "time"
+              ? new Date(this.data["time"].Data[0]).valueOf()
+              : parseFloat(this.data["distance"].Data[0]),
+          max:
+            this.type === "time"
+              ? new Date(
+                  this.data["time"].Data[this.data["time"].Data.length - 1],
+                ).valueOf()
+              : parseFloat(
+                  this.data["distance"].Data[
+                    this.data["distance"].Data.length - 1
+                  ],
+                ),
+          ticks: {
+            callback: (val: number) => {
+              if (this.type === "distance") {
+                return `${val % 1 ? val.toFixed(1) : val} ${
+                  this.preferredUnits.distance
+                }`;
+              }
+
+              return new Date(val as number).toTimeString().substr(0, 5);
+            },
+          },
+        },
+        ...Object.fromEntries(
+          Object.keys(metricSettings)
+            .map((metric) => {
+              if (metricSettings[metric].yaxis === false) {
+                return [];
+              }
+
+              return [
+                metric,
+                {
+                  display: !metricSettings[metric].hiddenByDefault,
+                  position: "left",
+                  ...metricSettings[metric].yaxis,
+                  ticks: {
+                    callback: (val) => {
+                      const settings = metricSettings[metric];
+                      if (settings.formatterYaxis) {
+                        return settings.labelFormatter
+                          ? settings.labelFormatter(val as number)
+                          : settings.formatter(val as number);
+                      }
+                      return val;
+                    },
+                  },
+                },
+              ];
+            })
+            .filter((e) => e.length > 0),
+        ),
+      },
+      elements: {
+        point: {
+          radius: 0,
+        },
+      },
+      interaction: {
+        mode: "index",
+        intersect: false,
+      },
+      onHover: (_, i) => {
+        const index = i[0]?.index;
+        const p = this.data["position"].Data[index];
+        if (p) {
+          const el = document.createElement("div");
+          el.setAttribute("data-lat", p[0]);
+          el.setAttribute("data-lng", p[1]);
+
+          this.map.setMarker(el);
+        }
+      },
+      plugins: {
+        decimation: {
+          enabled: true,
+          algorithm: "lttb",
+        },
+        legend: {
+          display: true,
+          onClick: (e, legendItem, legend) => {
+            const meta = legend.chart.getDatasetMeta(legendItem.datasetIndex);
+            meta.hidden = meta.hidden === null ? !legendItem.hidden : null;
+            legend.chart.options.scales[meta.yAxisID].display =
+              legendItem.hidden;
+            legend.chart.update();
+          },
+        },
+        tooltip: {
+          callbacks: {
+            title: (tooltipItems) => {
+              if (!tooltipItems[0]) {
+                return;
+              }
+
+              const x = tooltipItems[0].parsed.x;
+              if (this.type === "distance") {
+                return `${x.toFixed(2)} ${this.preferredUnits.distance}`;
+              }
+              return new Date(x).toTimeString().substr(0, 5);
+            },
+            label: (tooltipItem) => {
+              const settings = metricSettings[tooltipItem.dataset.yAxisID];
+              let value = tooltipItem.formattedValue;
+              if (settings && settings.formatter) {
+                value = settings.formatter(tooltipItem.raw);
+              }
+
+              return `${tooltipItem.dataset.label}: ${value}`;
+            },
+          },
+        },
+        zoom: {
+          limits: {
+            x: { min: "original", max: "original" },
+            y: { min: "original", max: "original" },
+          },
+          zoom: {
+            drag: {
+              enabled: true,
+            },
+            wheel: {
+              enabled: true,
+            },
+            mode: "x",
+          },
+        },
+      },
+    };
   }
 
   private getMetricSettings() {
@@ -322,7 +396,6 @@ export class WorkoutStats extends LitElement {
         formatter: (val) => `${val ?? "-"} ${this.preferredUnits.heartRate}`,
         formatterYaxis: true,
         hiddenByDefault: true,
-        yaxis: { min: 0 },
       },
       cadence: {
         formatter: (val) => `${val ?? "-"} ${this.preferredUnits.cadence}`,
