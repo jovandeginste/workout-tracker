46bc926bb0918a9993b9f843eaaedbe0169f2b19
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2026-03-24 14:12:58 +0100

    chore(cleanup): Remove vendored files from Git
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- vendor/github.com/anyappinc/fitbit/doc.go
+++
@@ -1,6 +0,0 @@
-// package fitbit provides the client to interact with Fitbit Web API.
-//
-// Web API Reference: https://dev.fitbit.com/build/reference/web-api/
-//
-// This package supports Authorization Code Grant Flow with Proof Key for Code Exchange (PKCE).
-package fitbit
--- vendor/github.com/galeone/tcx/README.md
+++
@@ -1,3 +0,0 @@
-Garmin *.tcx parsing for Go
-===========================
-Some basic utilities for marshaling and unmarshaling Garmin *.tcx (xml) in to and out of Go structs.
