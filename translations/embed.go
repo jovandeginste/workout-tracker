103d32a665575e02d0018461337ab2c8be16f90f
Jo Vandeginste <bugs.github@dwarfy.be>
2026-05-22 20:04:26 +0200

    Revert "fix: wrap translations FS to dynamically add top-level language key"

--- translations/embed.go
+++ translations/embed.go
@@ -1,11 +1,8 @@
 package apptranslations
 
 import (
-	"bytes"
 	"embed"
 	"io/fs"
-	"path/filepath"
-	"strings"
 
 	"github.com/labstack/echo/v4"
 )
@@ -13,42 +10,6 @@ import (
 //go:embed *
 var embedded embed.FS
 
-type rewriteFS struct {
-	fs.FS
-}
-
-func (r rewriteFS) ReadFile(name string) ([]byte, error) {
-	data, err := fs.ReadFile(r.FS, name)
-	if err != nil {
-		return nil, err
-	}
-
-	ext := filepath.Ext(name)
-	if ext != ".yaml" && ext != ".yml" {
-		return data, nil
-	}
-
-	base := filepath.Base(name)
-	lang := strings.TrimSuffix(base, ext)
-	lang = strings.ReplaceAll(lang, "_", "-")
-
-	lines := strings.Split(string(data), "\n")
-	var buf bytes.Buffer
-	buf.WriteString(lang + ":\n")
-	for _, line := range lines {
-		if strings.TrimSpace(line) == "---" {
-			continue
-		}
-		if line == "" {
-			buf.WriteString("\n")
-		} else {
-			buf.WriteString("  " + line + "\n")
-		}
-	}
-	return buf.Bytes(), nil
-}
-
 func FS() fs.FS {
-	subFS := echo.MustSubFS(embedded, "")
-	return rewriteFS{subFS}
+	return echo.MustSubFS(embedded, "")
 }
